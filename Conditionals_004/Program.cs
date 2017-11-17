984e99bda4d14066bef968d78e7cb1bf628ec19c
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-18 00:00:24 +1100

    First commit

---
+++ Conditionals_004/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Conditionals_004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your age? ");
+            string ageAsString = Console.ReadLine();
+            int age = int.Parse(ageAsString);
+
+            if (age > 65)
+            {
+                Console.WriteLine("You are a senior citizen.");
+            }
+            else if ( age >= 21 )
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else 
+            {
+                Console.WriteLine("You are not yet a " +
+                                  "legal adult.");
+            }
+        }
+    }
+}
